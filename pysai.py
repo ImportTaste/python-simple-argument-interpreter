a4dd0f82d308baee4f891a009d77145c50d98d49
Spazzlo <milo.tynan@stoaks.org>
2017-03-24 16:30:58 -0700

    Update!

--- pysai.py
+++ pysai.py
@@ -1,11 +1,33 @@
 def option(argv, optiontoparsefor):
-	found = False
 	for i in range(1, len(argv)):
 		if argv[i] == optiontoparsefor:
 			return argv[i + 1]
 def flag(argv, optiontoparsefor):
-	found = False
 	for i in range(1, len(argv)):
 		if argv[i] == optiontoparsefor:
-			found = True
-		return found
+			return True
+	return False
+def missingFlags(argv, optionstoparsefor):
+	founds = 0
+	for i in range(0, len(optionstoparsefor)):
+		for o in range(1, len(argv)):
+			if argv[o] == optionstoparsefor[i]:
+				founds = founds + 1
+	if founds == len(optionstoparsefor):
+		return True
+	return False
+
+def missingOptions(argv, optionstoparsefor):
+	founds = 0
+	misc = "misc"
+	for i in range(0, len(optionstoparsefor)):
+		for o in range(1, len(argv)):
+			if argv[o] == optionstoparsefor[i]:
+				try:
+					misc = argv[o + 1]
+				except IndexError:
+					return True
+				founds = founds + 1
+	if founds == len(optionstoparsefor):
+		return False
+	return True
